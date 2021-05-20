ec65c86e6e0c0db7c5283a4669360d571611ae3a
Alex3236 <45303195+eagle3236@users.noreply.github.com>
2021-05-20 13:09:13 +0800

    优化代码

--- Hat.py
+++ Hat.py
@@ -15,39 +15,32 @@ PLUGIN_METADATA = {
 
 @new_thread(PLUGIN_METADATA['name'])
 def put_on(source: CommandSource):
-	if isinstance(source, PlayerCommandSource):
-		api = source.get_server().get_plugin_instance('minecraft_data_api')
-		
-		selected_slot = api.get_player_info(source.player,'SelectedItemSlot')
-		selected_slotinfo = [i for i in api.get_player_info(source.player,'Inventory') if i['Slot'] == selected_slot]
-		if not len(selected_slotinfo):
-			selected_slotinfo = None
-		
-		head_slotinfo = [i for i in api.get_player_info(source.player,'Inventory') if i['Slot'] == 103]
-		if not len(head_slotinfo):
-			head_slotinfo = None
+	api = source.get_server().get_plugin_instance('minecraft_data_api')
 
-		item1 = slot_item_decode(selected_slotinfo[0])
-		item2 = slot_item_decode(head_slotinfo[0])
-		slot1 = 'container.'+str(selected_slot)
-		slot2 = 'armor.head'
+	selected_slot = api.get_player_info(source.player,'SelectedItemSlot')
+	selected_slotinfo = [i for i in api.get_player_info(source.player,'Inventory') if i['Slot'] == selected_slot]
+	if not len(selected_slotinfo):
+		selected_slotinfo = None
 
-		execute = source.get_server().execute
-		if not item1 and not item2:
-			return
-		elif not item1:
-			execute(pack_repitem(source.player, slot1, item2))
-			execute(pack_repitem(source.player, slot2, item_air()))
-		elif not item2:
-			execute(pack_repitem(source.player, slot1, item_air()))
-			execute(pack_repitem(source.player, slot2, item1))
-		else:
-			execute(pack_repitem(source.player, slot2, item1))
-			execute(pack_repitem(source.player, slot1, item2))
+	head_slotinfo = [i for i in api.get_player_info(source.player,'Inventory') if i['Slot'] == 103]
+	if not len(head_slotinfo):
+		head_slotinfo = None
+
+	item1 = slot_item_decode(selected_slotinfo[0])
+	item2 = slot_item_decode(head_slotinfo[0])
+	if not item1:
+		item1 = item_air()
+	if not item2:
+		item2 = item_air()
+	slot1 = 'container.'+str(selected_slot)
+	slot2 = 'armor.head'
+
+	execute = source.get_server().execute
+	execute(pack_repitem(source.player, slot2, item1))
+	execute(pack_repitem(source.player, slot1, item2))
 
 def pack_repitem(player, slot, item):
-        return 'replaceitem entity ' + player + ' ' + str(slot) + ' ' + item[
-            'id'] + item['tag'] + ' ' + str(item['count'])
+        return f'replaceitem entity {player} {str(slot)} {item['id']}{item['tag']} {str(item['count'])}'
 
 def slot_item_decode(info):
         count = info['Count']
@@ -59,5 +52,6 @@ def item_air():
         return {'id': 'minecraft:air', 'count': 1, 'tag': ''}
 
 def on_load(server: ServerInterface, prev):
+	check_and_put = lambda src: put_on(source) if src.is_player else src.reply('§c該命令只能被玩家使用。')
 	server.register_help_message('!!hat', '把手上的物品戴到頭上')
-	server.register_command(Literal('!!hat').runs(put_on))
+	server.register_command(Literal('!!hat').runs(check_and_put))
